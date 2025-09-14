bb3b7c7e38ecdb736902e57e807e04f79fed50c7
onojaMatthew <onojamatthew59@gmail.com>
2025-09-14 02:40:54 +0100

    fixed babel issues with docker^compose run

--- order-service/src/config/key.js
+++ order-service/src/config/key.js
@@ -3,7 +3,7 @@ import { config } from "dotenv"
 config();
 
 export const key = {
-  MONGO_URL: process.env.MONGO_URL,
+  MONGO_URL: process.env.MONGODB_URI,
   PRODUCT_SERVICE_URL: process.env.PRODUCT_SERVICE_URL,
   CUSTOMER_SERVICE_URL: process.env.CUSTOMER_SERVICE_URL,
   PAYMENT_SERVICE_URL: process.env.PAYMENT_SERVICE_URL,
--- order-service/src/server.js
+++ order-service/src/server.js
@@ -14,7 +14,7 @@ const swaggerJSDoc = YAML.load(path.resolve(__dirname, "../api.yaml"));
 
 const app = express();
 
-const port = process.env.PORT || 4400
+const port = process.env.PORT || 5400
 
 const startApp = async () =>  {
   try {
